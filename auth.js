4e07a40a718a488cf736869602742aed0be0742b
Adam Asshidiq <a.asshidiq@gmail.com>
2018-09-05 00:28:10 +0700

    masked consumer key, secret and access token, secret

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: "E0ScrtLQZGuCJ4atRsYum4eyX",
-  consumer_secret: "hwPEzRqLNtDEB2bR0c0Tr9jQxdFxJ6k2IUEXaeqPMReAqWbEOR",
-  access_token_key: "110956479-f5eaCvtIiLMxyQXSaBbpaH0CVgDdRsd8lQowx2Zs",
-  access_token_secret: "OeFGUojWM9aEIffJObY780YS1tNBImj2oI2fvsmZOSEkl"
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 }
